9da1efef3bbded9df53ebf23a8d5be864d9bc74f
minnibaevazarina <105646907+minnibaevazarina@users.noreply.github.com>
2022-06-23 17:11:53 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,7 +1,7 @@
 import time
 from selenium.webdriver.common.by import By
 
-def test_items(browser):
+def test_button_add_to_cart_exist(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     time.sleep(30)
