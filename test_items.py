f51109b5bb0ced61581c0085ad64d097f5ca253f
MinnibaevaZ <minnibaeva.zarina@mail.ru>
2022-06-23 16:42:43 +0300

    Commit1

---
+++ test_items.py
@@ -0,0 +1,9 @@
+import time
+from selenium.webdriver.common.by import By
+
+def test_items(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    time.sleep(30)
+    button = browser.find_element(By.XPATH, "//button[contains(@class, 'basket')]")
+    assert button, "button is missing"
